8f4a0a0eab8013ddcd82dd015b417a89f9fb3fb3
Taoshuaifei <57394350+Taoshuaifei@users.noreply.github.com>
2023-05-19 00:47:48 +0800

    Update index.vue

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -481,7 +481,7 @@ onUnmounted(() => {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>Aha~</span>
+              <span>哈？今儿想问点啥子~~</span>
             </div>
           </template>
           <template v-else>
